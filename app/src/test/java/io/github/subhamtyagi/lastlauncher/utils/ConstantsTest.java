acd1ddea6658815fc0e75b1127783a0630290198
Shubham <shubhamtyagi.bajoat@gmail.com>
2022-04-24 14:51:18 +0530

    Revert "some comment"
    This reverts commit 881d64481ed9839eb0b3a6469d43bcef9f9ebc2f.

--- app/src/test/java/io/github/subhamtyagi/lastlauncher/utils/ConstantsTest.java
+++ app/src/test/java/io/github/subhamtyagi/lastlauncher/utils/ConstantsTest.java
@@ -13,15 +13,13 @@ public class ConstantsTest {
         constantsUnderTest = new Constants();
     }
 
-    //CS304 Issue link: https://github.com/SubhamTyagi/Last-Launcher/issues/162
     @Test
     public void testSortBySize() {
         Assert.assertEquals(Constants.SORT_BY_SIZE, 2);
     }
 
-    //CS304 Issue link: https://github.com/SubhamTyagi/Last-Launcher/issues/162
     @Test
-    public void testSortByRecentOpen() {
-        Assert.assertEquals(Constants.SORT_BY_RECENT_OPEN, 7);
+    public void testSortByRecentOpen(){
+        Assert.assertEquals(Constants.SORT_BY_RECENT_OPEN,7);
     }
 }
